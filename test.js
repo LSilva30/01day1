4603c07498dd0a49cec88b01e47f073a43e92f8c
Luiz Silva <luizsilva@Luizs-MacBook-Pro.local>
2021-06-30 11:13:54 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+//const myName = 'Luiz'
+
+//console.log('Hello ' + myName)
+
+
+//define a function for isDrinkingAge
+//given an age -- say if someone is drinking age or not
+
+function isDrinkingAge(age) {
+    let message = (age < 21) ? 'Not Drinking Age' : 'Legal Drinking age'
+    // if (age < 21) {
+    // return console.log('Not Drinking Age')
+    // } else {
+    //     return console.log('Legal Drinkning age')
+    // }
+    console.log(message)
+}
+let userAge = 21
+isDrinkingAge(userAge)
+
+
+
---
+++ test2.js
@@ -0,0 +1,40 @@
+// var address = 'Boca Raton'
+// let myAge = 25
+// let year = 1980
+
+// const myName = 'Luiz'
+
+// console.log('Hey this is my name' , myName , myAge, address)
+
+// myAge = 26
+// console.log(myAge, myName)
+
+
+
+
+//working with functions ------------------------------------------------
+
+
+// function calculateAge() {
+//     return 2 + 2
+// }
+
+// function test(){
+//     console.log(10 + 2)
+// }
+
+
+// console.log(calculateAge())
+// test()
+
+const todaysYear = 2021
+let toddsYear = 1975
+let luizYear = 1995
+
+function calculateAge(birthYear){
+    console.log(todaysYear - birthYear)
+}
+
+calculateAge(toddsYear)
+calculateAge(luizYear)
+calculateAge(2003)
